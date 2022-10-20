795fee8d265419b84dd20a04595cdf05056e8121
Tang <mrgaoyi@gmail.com>
2022-10-20 10:08:42 +0700

    add small changes

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -16,3 +16,4 @@ bufferline.setup {
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set('n', '<leader>c', '<Cmd>bdelete<CR>', { silent = true, desc = "delete buffer" })
--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -1,11 +1,34 @@
-local status, telescope = pcall(require, "telescope")
-if (not status) then return end
-local status2, builtin = pcall(require, 'telescope.builtin')
-if (not status2) then return end
+-- import telescope plugin safely
+local telescope_setup, telescope = pcall(require, "telescope")
+if not telescope_setup then return end
+
+-- import telescope actions safely
+local actions_setup, actions = pcall(require, "telescope.actions")
+if not actions_setup then return end
+
+local builtin_setup, builtin = pcall(require, 'telescope.builtin')
+if not builtin_setup then return end
+
+-- configure telescope
+telescope.setup({
+  -- configure custom mappings
+  defaults = {
+    mappings = {
+      i = {
+        -- ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+        -- ["<C-j>"] = actions.move_selection_next, -- move to next result
+        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+      },
+    },
+  },
+})
+
+telescope.load_extension("fzf")
 
 telescope.setup {}
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
